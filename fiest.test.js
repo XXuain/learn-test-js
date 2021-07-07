0d04174a3e4ed87516d84804d0f639c1ef07adc2
yellowHuang <hlady1115@gmail.com>
2021-07-07 13:58:06 +0800

    add: keywork try

--- fiest.test.js
+++ fiest.test.js
@@ -1,1 +1,20 @@
-test("first", () => {});
+describe("Our test block", () => {
+  test("First", () => {
+    expect(3 + 3).toBe(6);
+    expect(3 + 3).toBe(6);
+  });
+
+  it("Second", () => {
+    expect(2 + 3).toBe(5);
+  });
+});
+
+describe("Our test block2", () => {
+  test("111", () => {
+    expect(3 + 3).toBe(6);
+  });
+
+  it("222", () => {
+    expect(2 + 3).toBe(5);
+  });
+});
